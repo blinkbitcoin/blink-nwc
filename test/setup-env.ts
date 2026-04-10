200a0135fc74b76242b30aae939a581825ff2007
basarrcan <basarrcan@gmail.com>
2026-04-10 23:36:50 +0300

    fix(test): use repo database defaults in setup env

--- test/setup-env.ts
+++ test/setup-env.ts
@@ -1,4 +1,7 @@
 process.env.NOSTR_PRIVATE_KEY ??= "1".repeat(64)
 process.env.DATA_ENCRYPTION_KEY ??= "0".repeat(64)
-process.env.DB_USER ??= "test-db-user"
-process.env.DB_PWD ??= "test-db-password"
+process.env.DB_HOST ??= "localhost"
+process.env.DB_PORT ??= "5435"
+process.env.DB_USER ??= "blink-nwc-usr"
+process.env.DB_PWD ??= "blink-nwc-pwd"
+process.env.DB_DB ??= "blink-nwc"
